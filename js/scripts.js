aef0854819e1d08c9866ecd39d6f27defe596301
Student <example@example.com>
2018-08-10 11:07:04 -0700

    add js functionality to true-false section

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
-  
+  $("#tf").submit(function(event) {
+    event.preventDefault();
+
+    var n = 0;
+    $("input:radio:checked").each(function() {
+      n += parseInt($(this).val());
+    });
+    console.log(n);
+
+    if (n == 0) {
+      $("#hidden-s").show();
+      $("#lang-survey").show();
+    }
+    if (n > 0) {
+      $("#hidden-f").show();
+    }
+    if (n < 0) {
+      $("#hidden-b").show();
+      $("#lang-survey").show();
+    }
+    $("#tf-btn").hide();
+  })
+
+  $("#lang-survey").submit(function() {
+
+  })
 })
