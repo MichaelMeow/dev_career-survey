2d1ce078c9216cb1d31657d06ecca909443ba43d
Student <example@example.com>
2018-08-10 11:59:20 -0700

    sort languages by most relevant

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $("#tf").submit(function(event) {
     event.preventDefault();
+    $("#tf-btn").hide();
 
     var n = 0;
     $("input:radio:checked").each(function() {
@@ -19,10 +20,62 @@ $(document).ready(function(){
       $("#hidden-b").show();
       $("#lang-survey").show();
     }
-    $("#tf-btn").hide();
   })
 
+
+
   $("#lang-survey").submit(function() {
+    event.preventDefault();
+    $("#tf-btn").hide();
+    // multipe answers displayed, and sorted based on most commonly checked relevant answers
+
+
+    var n = "";
+    $("input:checkbox:checked").each(function() {
+      n += $(this).val();
+      console.log(n);
+    });
+
+    var c = []
+    var java = []
+    var php = []
+    var ruby = []
+    var langs = [c,java,php,ruby]
+
+    if (n.includes("c")){
+      c.push((n.match(/c/g)).length, "c");
+    } else{
+      c.push(0, c);
+    }
+
+    if (n.includes("java")){
+      java.push((n.match(/java/g)).length, "java");
+
+    } else{
+      java.push(0, java);
+    }
+
+    if (n.includes("php")){
+      php.push((n.match(/php/g)).length, "php");
+    } else{
+      php.push(0, php);
+    }
+
+    if (n.includes("ruby")){
+      ruby.push((n.match(/ruby/g)).length, "ruby");
+    } else{
+      ruby.push(0, "ruby");
+    }
+    console.log(langs);
+    console.log(langs[0]);
+    langs.sort(function(a, b)
+{
+    return b[0] - a[0];
+});
+
+    console.log(langs);
+    console.log(langs[0]);
+
 
   })
 })
