1ff5df65ed94851d9d1cfc7bbcd701dbac1e9a8f
MichaelMeow <dearmichaelmeow@gmail.com>
2018-08-16 13:51:05 -0700

    add semicolons to js, rename variable names to be more descriptive, and add my name to commits using git config

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,24 @@ $(document).ready(function(){
 
 
   // when submit is clicked the following will add to n for front end and subtract for back end relevance, and n will determine the result
-  $("#tf").submit(function(event) {
+  $("#trueFalse").submit(function(event) {
     event.preventDefault();
-    $("#tf-btn").hide();
+    $("#trueFalseButton").hide();
     $("input:radio").attr('disabled',true);
     var n = 0;
     $("input:radio:checked").each(function() {
       n += parseInt($(this).val());
     });
     if (n == 0) {
-      $("#hidden-s").show();
+      $("#hidden-stack").show();
       $("#lang-survey").show();
     }
     if (n > 0) {
-      $("#hidden-f").show();
+      $("#hidden-front").show();
       $("#end-message").show();
     }
     if (n < 0) {
-      $("#hidden-b").show();
+      $("#hidden-back").show();
       $("#lang-survey").show();
     }
     window.scrollBy(0,1000);
@@ -31,44 +31,44 @@ $(document).ready(function(){
     $("#lang-answer").show();
     $("#end-message").show();
     $("input:checkbox").attr('disabled',true);
-    var a = "";
+    var checkedAnswerValues = "";
     $("input:checkbox:checked").each(function() {
-      a += $(this).val();
+      checkedAnswerValues += $(this).val();
     });
     // this creates an array with arrays storing relevant checkbox ticks for each language
-    var c = []
-    var java = []
-    var php = []
-    var ruby = []
-    var langs = [c,java,php,ruby]
+    var cArray = [];
+    var javaArray = [];
+    var phpArray = [];
+    var rubyArray = [];
+    var langsArray = [cArray,javaArray,phpArray,rubyArray];
 
-    if (a.includes("c")){
-      c.push((a.match(/c/g)).length, "c");
+    if (checkedAnswerValues.includes("c")){
+      cArray.push((checkedAnswerValues.match(/c/g)).length, "c");
     } else{
-      c.push(0, c);
+      cArray.push(0, "c");
     }
-    if (a.includes("java")){
-      java.push((a.match(/java/g)).length, "java");
+    if (checkedAnswerValues.includes("java")){
+      javaArray.push((checkedAnswerValues.match(/java/g)).length, "java");
     } else{
-      java.push(0, java);
+      javaArray.push(0, "java");
     }
-    if (a.includes("php")){
-      php.push((a.match(/php/g)).length, "php");
+    if (checkedAnswerValues.includes("php")){
+      phpArray.push((checkedAnswerValues.match(/php/g)).length, "php");
     } else{
-      php.push(0, "php");
+      phpArray.push(0, "php");
     }
-    if (a.includes("ruby")){
-      ruby.push((a.match(/ruby/g)).length, "ruby");
+    if (checkedAnswerValues.includes("ruby")){
+      rubyArray.push((checkedAnswerValues.match(/ruby/g)).length, "ruby");
     } else{
-      ruby.push(0, "ruby");
+      rubyArray.push(0, "ruby");
     }
-    langs.sort(function(a, b)
-    {
+    // a and b are for comparison for which value is greater
+    langsArray.sort(function(a, b){
       return b[0] - a[0];
     });
-    var plural = 0
+    var plural = 0;
     // this will append each answer into #lang-answer in order of amount of relevant checkboxes clicked, using data stored in the arrays
-    $(langs).each(function(){
+    $(langsArray).each(function(){
       if (this[0] == 0){
         // do nothing
       } else{
